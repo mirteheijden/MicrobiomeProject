ef2049f77fb26d39881f29766c10b0c01dfb3c56
mirteheijden <mirteheijden@hotmail.com>
2025-07-08 08:21:10 +0300

    Update taxprofiler_part1.sh

--- mice_samples/Preprocessing/SCRIPTS/taxprofiler_part1.sh
+++ mice_samples/Preprocessing/SCRIPTS/taxprofiler_part1.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=72:00:00
 #SBATCH --partition=small
 #SBATCH --ntasks=4
-#SBATCH --account=project_2008347
+#SBATCH --account=project_XXXX
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=120G
 
@@ -24,7 +24,7 @@ nextflow run nf-core/taxprofiler -r 1.1.5 -resume \
    --perform_shortread_qc --save_preprocessed_reads\
    --perform_shortread_complexityfilter  --shortread_complexityfilter_tool bbduk \
    --perform_shortread_hostremoval \
-   --hostremoval_reference /scratch/project_2008347/mice_feces_IV2/DB/mice.fasta \
+   --hostremoval_reference /scratch/project_XXXX/mice_feces_IV2/DB/mice.fasta \
    --save_hostremoval_unmapped \
    --save_analysis_ready_fastqs \
    --run_profile_standardisation
