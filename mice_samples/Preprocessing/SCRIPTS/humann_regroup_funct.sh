8682a59bc45ce18c8ffcf7022db1434d681bb772
mirteheijden <mirteheijden@hotmail.com>
2025-07-08 08:18:26 +0300

    Update humann_regroup_funct.sh

--- mice_samples/Preprocessing/SCRIPTS/humann_regroup_funct.sh
+++ mice_samples/Preprocessing/SCRIPTS/humann_regroup_funct.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=10:00:00          #adjust for the time needed
 #SBATCH --partition=small        #for multinode, large partition should be use
 #SBATCH --nodes=1                #max nodes allowed 26
-#SBATCH --account=project_2008347
+#SBATCH --account=project_XXXX
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=80G
@@ -16,48 +16,48 @@
 echo "start"
 
 # Define the parent dir
-parent="/scratch/project_2008347/mice_feces_IV2"
+parent="/scratch/project_XXXX/mice_feces_IV2"
 module load humann/3.8
 input="$parent/RESULTS/humann3/merged/genefamilies.txt"
 
 # uniref90-ko
 regroup_KO() {
         output="$parent/RESULTS/humann3/final/genefamilies_Uniref90_KO.txt"
-        DB="/scratch/project_2008347/mice_feces_IV2/DATABASE_DIR/utility_mapping/map_ko_uniref90.txt.gz"
+        DB="/scratch/project_XXXX/mice_feces_IV2/DATABASE_DIR/utility_mapping/map_ko_uniref90.txt.gz"
         humann_regroup_table --input "$input" --output "$output" -c $DB
 }
 
 # uniref90-EggNOG
 regroup_eggnog() {
         output="$parent/RESULTS/humann3/final/genefamilies_Uniref90_EggNOG.txt"
-        DB="/scratch/project_2008347/mice_feces_IV2/DATABASE_DIR/utility_mapping/map_eggnog_uniref90.txt.gz"
+        DB="/scratch/project_XXXX/mice_feces_IV2/DATABASE_DIR/utility_mapping/map_eggnog_uniref90.txt.gz"
         humann_regroup_table --input "$input" --output "$output" -c $DB
 }
 
 # uniref90-GO
 regroup_GO() {
         output="$parent/RESULTS/humann3/final/genefamilies_Uniref90_GO.txt"
-        DB="/scratch/project_2008347/mice_feces_IV2/DATABASE_DIR/utility_mapping/map_go_uniref90.txt.gz"
+        DB="/scratch/project_XXXX/mice_feces_IV2/DATABASE_DIR/utility_mapping/map_go_uniref90.txt.gz"
          humann_regroup_table --input "$input" --output "$output" -c $DB
 }
 
 # uniref90-Pfam
 regroup_pfam() {
         output="$parent/RESULTS/humann3/final/genefamilies_Uniref90_Pfam.txt"
-        DB="/scratch/project_2008347/mice_feces_IV2/DATABASE_DIR/utility_mapping/map_pfam_uniref90.txt.gz"
+        DB="/scratch/project_XXXX/mice_feces_IV2/DATABASE_DIR/utility_mapping/map_pfam_uniref90.txt.gz"
         humann_regroup_table --input "$input" --output "$output" -c $DB
 }
 
 # uniref90-EC
 regroup_ec() {
         output="$parent/RESULTS/humann3/final/genefamilies_Uniref90_EC.txt"
-        DB="/scratch/project_2008347/mice_feces_IV2/DATABASE_DIR/utility_mapping/map_level4ec_uniref90.txt.gz"
+        DB="/scratch/project_XXXX/mice_feces_IV2/DATABASE_DIR/utility_mapping/map_level4ec_uniref90.txt.gz"
         humann_regroup_table --input "$input" --output "$output" -c $DB
 }
 # uniref90-MetaCyc
 regroup_metacyc() {
         output="$parent/RESULTS/humann3/final/genefamilies_Uniref90_MetaCyc.txt"
-        DB="/scratch/project_2008347/mice_feces_IV2/DATABASE_DIR/utility_mapping/map_level4ec_uniref90.txt.gz"
+        DB="/scratch/project_XXXX/mice_feces_IV2/DATABASE_DIR/utility_mapping/map_level4ec_uniref90.txt.gz"
         humann_regroup_table --input "$input" --output "$output" -c $DB -g uniref90_rxn
 }
 
