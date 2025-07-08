67e54992c98cf933abcaae4efa8c323d8059c6b1
mirteheijden <mirteheijden@hotmail.com>
2025-07-08 08:20:27 +0300

    Update run_GG2shotgun_workflow.sh

--- mice_samples/Preprocessing/SCRIPTS/run_GG2shotgun_workflow.sh
+++ mice_samples/Preprocessing/SCRIPTS/run_GG2shotgun_workflow.sh
@@ -8,7 +8,7 @@ source ~/.bash_profile
 
 
 snakemake  -s ./workflow/Snakefile_gg2_shotgun  --cluster \
-    " sbatch --account=project_2008347 \
+    " sbatch --account=project_XXXX \
     --partition small --mem 150G --time=3-00:00:00 \
     --ntasks=1 --cpus-per-task=32 \
      -e LOGS/shotgun_err_%A_%a.txt" -j 400 \
